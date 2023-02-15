400fc3889c0ef4af76e11b1976b160b56b647c23
z20axa <alejandrospan@hotmail.com>
2023-02-15 17:17:44 -0500

    Adds revisions to the resolver and typeDefs codes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,6 @@ const { User } = require('../models');
 const {AuthenticationError} = require('apollo-server-express');
 const {signToken} = require('../utils/auth');
 
-
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
