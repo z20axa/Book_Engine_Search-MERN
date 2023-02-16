dd5d60af982ec5d005c824b801062da6f8b58544
z20axa <alejandrospan@hotmail.com>
2023-02-15 22:10:47 -0500

    Revises server js code to set up the appollo server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
+// packages/modules imports
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-// require('dotenv').config();
-
 const { ApolloServer } = require('@apollo/server');
 const { authMiddleware } = require('./utils/auth');
 
+// setting up appollo middleware
 const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
 const cors = require('cors');
 
-const app = express();
+
+// const app = express();
+
+// port set up
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // app.use(routes);
 
