adb616084a5aec2e549786c9c053c72e411a1e21
z20axa <alejandrospan@hotmail.com>
2023-02-15 21:32:54 -0500

    Adds finish code for server resolver js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,61 +23,54 @@ Mutation: {
         return {user, token};
     },
 
-    // log in
-    // login: async (parent, { email, password }) => {
-    //   const user = await User.findOne({ email });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-    //   if (!user) {
-    //     throw new AuthenticationError('No user found with this email address');
-    //   }
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
 
-    //   const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-    //   if (!correctPw) {
-    //     throw new AuthenticationError('Incorrect credentials');
-    //   }
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-    //   const token = signToken(user);
+      const token = signToken(user);
 
-    //   return { token, user };
-    // },
+      return { token, user };
+    },
 
-    // save book
-    // addComment: async (parent, { thoughtId, commentText }, context) => {
-    //   if (context.user) {
-    //     return Thought.findOneAndUpdate(
-    //       { _id: thoughtId },
-    //       {
-    //         $addToSet: {
-    //           comments: { commentText, commentAuthor: context.user.username },
-    //         },
-    //       },
-    //       {
-    //         new: true,
-    //         runValidators: true,
-    //       }
-    //     );
-    //   }
-    //   throw new Error('You need to be logged in!');
-    // },
+    
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              saveBooks: bookData,
+            },
+          },
+          {
+            new: true
+          }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
 
-    // remove book
-    // removeThought: async (parent, { thoughtId }, context) => {
-    //   if (context.user) {
-    //     const thought = await Thought.findOneAndDelete({
-    //       _id: thoughtId,
-    //       thoughtAuthor: context.user.username,
-    //     });
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndDelete(
+          {_id: context.user._id},
+          { $pull: { saveBooks: {bookId} } },
+          {new: true}
+      );
 
-    //     await User.findOneAndUpdate(
-    //       { _id: context.user._id },
-    //       { $pull: { thoughts: thought._id } }
-    //     );
-
-    //     return thought;
-    //   }
-    //   throw new AuthenticationError('You need to be logged in!');
-    // },
-},
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+}};
 
 module.exports = resolvers;
