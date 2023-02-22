f03ae9fcea1faf13539210b587be8a8d5fe35b0a
z20axa <alejandrospan@hotmail.com>
2023-02-21 20:17:06 -0500

    Adds server express route for REACT

--- server/server.js
+++ server/server.js
@@ -12,18 +12,24 @@ const { typeDefs, resolvers } = require('./schemas');
 const cors = require('cors');
 
 
-// const app = express();
+const app = express();
 
 // port set up
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// single express route to serve entire REACT front end
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
+
 
 // app.use(routes);
 
