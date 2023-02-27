31a4b611d887ad440eb593b20496b8cfde3aa553
z20axa <alejandrospan@hotmail.com>
2023-02-27 12:07:26 -0500

    Adds additinal comments to server js file

--- server/server.js
+++ server/server.js
@@ -1,78 +1,25 @@
 // // packages/modules imports
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-// const { ApolloServer } = require('@apollo-server-express');
-// const { authMiddleware } = require('./utils/auth');
-
-// // setting up appollo middleware
-// const { expressMiddleware } = require('@apollo/server/express4');
-// const { typeDefs, resolvers } = require('./schemas');
-// const cors = require('cors');
-
-
-// const app = express();
-
-// // port set up
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// // single express route to serve entire REACT front end
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/')); // render by default the index.html inside client/public 
-// })
-
-
-// // app.use(routes);
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware,
-// })
-
-// // db.once('open', () => {
-// //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// // });
-
-// const startServer = async() => {
-//   await server.start();
-//   app.use('/graphql', cors(), expressMiddleware(server));
-//   db.once('open', () => {
-//     app.listen(PORT, () => {
-//       console.log(`API server running on http://localhost:${PORT}`);
-//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-//     });
-//   });
-// }
-
-
-// startServer();
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+// setting computer port number for service and Heruko deployment
 const PORT = process.env.PORT || 3001;
+
+// initialize our app variable by setting it to the value of express()
 const app = express();
+
+// authentication middleware to work in teh context of a GraphQL API
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+// body parsing
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -81,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// home route for front end client
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
 })
