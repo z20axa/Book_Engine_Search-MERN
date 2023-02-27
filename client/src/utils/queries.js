fd251574ed401dda402408ffae3fb9c3d90c2cf6
z20axa <alejandrospan@hotmail.com>
2023-02-27 16:37:51 -0500

    Revises the apollo/client me query code format

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,20 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
-query me{
-  me {
-    _id
-    bookCount
-    email
-    savedBooks {
-      authors
-      bookId
-      description
-      image
-      link
-      title
+  query me{
+    me {
+      _id
+      bookCount
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      username
     }
-    username
   }
-}
 `;
